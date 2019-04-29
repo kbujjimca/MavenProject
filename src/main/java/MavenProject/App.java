900f3d860be7a4fdd2024b34bf22ce986a269ec9
bujji <bujji@bujjis-mbp.hitronhub.home>
2019-04-28 18:10:35 -0600

    Ok

--- src/main/java/MavenProject/App.java
+++ src/main/java/MavenProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Maven Test project" );
+        System.out.println( "This is Maven Test project.Running job on Jenkins" );
     }
 }
